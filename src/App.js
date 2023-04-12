a9876345ac68018b36a7999ac7b9bb0bb3666d6e
Niharika Chenna <114658642+NiharikaChenna@users.noreply.github.com>
2023-04-12 19:23:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
-// import AddContact from './components/AddContact';
-// import EditContact from './components/EditContact';
 import { ToastContainer } from "react-toastify";
 import Navbar from "./Header/Navbar";
 import UserProfile from "./components/UserProfile";
